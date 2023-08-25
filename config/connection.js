796dbeba1aead5128767cbb557b9b0a9afa51b7f
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-08-24 20:03:40 -0400

    connection redirected to port

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-mongoose.connect('mongodb://localhost/social-network', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Connecting to the MongoDB server 
+connect('mongodb://127.0.0.1:27017/social-network');
 
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB database');
-});
-
-module.exports = mongoose.connection;
+module.exports = connection;
